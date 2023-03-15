b7a292b0ef28bdb53a443a45185b93eba392bcf3
GiannosL <giannos.louloudis@gmail.com>
2023-03-15 17:23:31 +0100

    title page

--- pages/views/view_1_stayactive.py
+++ pages/views/view_1_stayactive.py
@@ -2,11 +2,12 @@ import streamlit as st
 
 from pages.utilities.my_utils import Utils
 
+
 class StayActiveView:
     @staticmethod
     def run() -> None:
         # multiple pages with wide view
-        st.set_page_config(page_title="DrugGraph", layout="wide")
+        st.set_page_config(page_title="StayActive", layout="wide")
 
         # remove watermark
         Utils.remove_watermark()
