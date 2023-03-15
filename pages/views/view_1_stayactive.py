7a30f1e51ad6c344baa3e36525a45f788a7f005f
GiannosL <giannos.louloudis@gmail.com>
2023-03-15 20:34:46 +0100

    metrics

--- pages/views/view_1_stayactive.py
+++ pages/views/view_1_stayactive.py
@@ -33,3 +33,12 @@ class StayActiveView:
 
         if update_btn:
             switch_page("personal info")
+
+        
+        st.write('## Health overview')
+        col1, col2, col3, col4, col5 = st.columns([1, 1, 1, 1, 1])
+        col1.metric(label="Heart rate", value="61 bpm", delta="1.2")
+        col2.metric(label="Sleep score", value="6h 17m", delta="22m")
+        col3.metric(label="Steps", value="5,242")
+        col4.metric(label="Calories burned", value="1,482 cal")
+        col5.metric(label="Days till fertility", value="10")
