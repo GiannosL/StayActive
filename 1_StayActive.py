8f0d304eec6cce86142cfda9fbe4199fee3f3c59
GiannosL <giannos.louloudis@gmail.com>
2023-03-15 17:21:51 +0100

    home page

---
+++ 1_StayActive.py
@@ -0,0 +1,7 @@
+
+from pages.views.view_1_stayactive import StayActiveView
+
+
+if __name__ == "__main__":
+    # show page
+    StayActiveView().run()
---
+++ pages/views/view_1_stayactive.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+from pages.utilities.my_utils import Utils
+
+class StayActiveView:
+    @staticmethod
+    def run() -> None:
+        # multiple pages with wide view
+        st.set_page_config(page_title="DrugGraph", layout="wide")
+
+        # remove watermark
+        Utils.remove_watermark()
+
+        # page title
+        st.write("# StayActive")
+
+        # 
+        st.write("## How are you feeling today?")
+        feeling_meter = st.slider("Physical state", 0, 10, 5)
