51b93dbe73544c31db183ccd2481512d511071f7
GiannosL <giannos.louloudis@gmail.com>
2023-03-15 21:38:51 +0100

    testing files

--- tests/test_personal_info.py
+++ tests/test_personal_info.py
@@ -1,5 +1,6 @@
 import os
 from pages.views.view_3_workoutdesign import load_info
+from pages.views.view_4_personalinfo import load_info as load_info4
 
 class TestFiles:
     def test_find_info(self):
@@ -11,3 +12,10 @@ class TestFiles:
     def test_number_of_entries(self):
         data = load_info()
         assert len(data) == 4
+
+    def test_info_variables(self):
+        variables = ['age', 'height', 'weight', 'fitness', 'health', 'preferences', 'place-id']
+        data = load_info4()
+
+        for var in variables:
+            assert var in data
